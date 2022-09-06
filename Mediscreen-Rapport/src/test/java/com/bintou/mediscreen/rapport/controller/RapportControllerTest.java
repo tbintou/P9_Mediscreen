c23ebde19e9d08f0a2a3e8b2523879f7d5228239
tbintou <48350302+tbintou@users.noreply.github.com>
2022-09-06 17:49:43 +0200

    MediscreenRapport testing

--- Mediscreen-Rapport/src/test/java/com/bintou/mediscreen/rapport/controller/RapportControllerTest.java
+++ Mediscreen-Rapport/src/test/java/com/bintou/mediscreen/rapport/controller/RapportControllerTest.java
@@ -2,4 +2,5 @@ package com.bintou.mediscreen.rapport.controller;
 
 public class RapportControllerTest {
 
+
 }
--- Mediscreen-Rapport/src/test/java/com/bintou/mediscreen/rapport/service/RapportServiceTest.java
+++ Mediscreen-Rapport/src/test/java/com/bintou/mediscreen/rapport/service/RapportServiceTest.java
@@ -1,22 +1,22 @@
 package com.bintou.mediscreen.rapport.service;
 
+import com.bintou.mediscreen.rapport.config.FeignBadResponseWrapper;
 import com.bintou.mediscreen.rapport.model.Note;
 import com.bintou.mediscreen.rapport.model.Patient;
-import com.bintou.mediscreen.rapport.proximity.NoteProximity;
-import com.bintou.mediscreen.rapport.proximity.PatientProximity;
+import com.bintou.mediscreen.rapport.model.Status;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -30,6 +30,10 @@ public class RapportServiceTest {
 
     @MockBean
     private PatientProximity patientProximity;*/
+
+    HttpHeaders headers = new HttpHeaders();
+    private Patient patient = new Patient();
+    private Note note = new Note();
 
     @Autowired
     private RapportService rapportService;
@@ -53,19 +57,44 @@ public class RapportServiceTest {
 
         Integer patientAge = rapportService.getPatientAge(patientInfo.getBirthDate());
         System.out.println("patientAge = " + patientAge);
-        Assertions.assertEquals(55, (int) patientAge);
+        assertEquals(77, (int) patientAge);
     }
 
+    @Test
+    public void getPatientInfoReturnNullTest() {
+       /*Long id = 40L;
+       patient.setId(id);
+       patient.setLastName("Jean");
+       patient.setFirstName("");
+       patient.setGender("");
+       patient.setBirthDate(LocalDate.ofEpochDay(40L));*/
 
+       RapportService rapportService1 = mock(RapportService.class);
+       when(rapportService1.getPatientInfo(40L))
+               .thenThrow(new FeignBadResponseWrapper(404, headers, "Bad request"));
+        assertNull(patient.getId());
+    }
 
     @Test
-    public void getPatientAssessmentTest() {
-        List<Note> noteList = rapportService.getPatientNote(9L);
+    public void getPatientAssessmentNoneTest() {
+        List<Note> noteList = rapportService.getPatientNote(1L);
         for (Note note : noteList) {
             System.out.println("note = " + note.getNote());
         }
-        String risk = rapportService.calculateRiskByPatientId(9L);
+        String risk = rapportService.calculateRiskByPatientId(1L);
         System.out.println("risque = " + risk);
-        Assertions.assertEquals("Aucun risque (None)", risk);
+        assertEquals("Aucun risque (None)", risk);
+    }
+
+    @Test
+    public void getPatientAssessmentBorderlineTest() {
+        List<Note> noteList = rapportService.getPatientNote(8L);
+        for (Note note : noteList) {
+            System.out.println("note = " + note.getNote());
+        }
+        String risk = rapportService.calculateRiskByPatientId(8L);
+        System.out.println("risque = " + risk);
+        assertEquals("Aucun risque (None)", risk);
+
     }
 }
