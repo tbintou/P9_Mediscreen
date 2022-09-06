ed29f658901630399b276deaf927ff67c8e95c22
tbintou <48350302+tbintou@users.noreply.github.com>
2022-09-06 12:08:39 +0200

    Test unit MediscreenRapport

--- Mediscreen-Rapport/src/test/java/com/bintou/mediscreen/rapport/service/RapportServiceTest.java
+++ Mediscreen-Rapport/src/test/java/com/bintou/mediscreen/rapport/service/RapportServiceTest.java
@@ -2,21 +2,51 @@ package com.bintou.mediscreen.rapport.service;
 
 import com.bintou.mediscreen.rapport.model.Note;
 import com.bintou.mediscreen.rapport.model.Patient;
+import com.bintou.mediscreen.rapport.proximity.NoteProximity;
+import com.bintou.mediscreen.rapport.proximity.PatientProximity;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class RapportServiceTest {
 
+    /*private Patient patient = new Patient();
+    private Note note = new Note();
+
+    @MockBean
+    private NoteProximity noteProximity;
+
+    @MockBean
+    private PatientProximity patientProximity;*/
+
     @Autowired
     private RapportService rapportService;
 
-    @Test
+
+  /*  @Test
+    public void testGetPatientAge() {
+        Integer patientAge = rapportService.getPatientAge(patientInfo.getBirthDate());
+        when(rapportService.getPatientInfo(1L)).thenReturn(LocalDate.ofEpochDay(1L));
+        Assertions.assertEquals(55, (int) patientAge);
+        verify(patient).getBirthDate();
+    }*/
+
+
+
+
+   @Test
     public void getPatientAgeTest() {
         Patient patientInfo = rapportService.getPatientInfo(1L);
         System.out.println("birthDate = " + patientInfo.getBirthDate());
@@ -26,7 +56,7 @@ public class RapportServiceTest {
         Assertions.assertEquals(55, (int) patientAge);
     }
 
-    
+
 
     @Test
     public void getPatientAssessmentTest() {
